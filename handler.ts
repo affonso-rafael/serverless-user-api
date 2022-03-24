ec84df15a0de8c17039daeeaa5852c1ec711411b
Rafael Affonso <hrafaelaf@gmail.com>
2022-03-24 12:30:43 -0300

    chore: add typescript

---
+++ handler.ts
@@ -0,0 +1,11 @@
+import { APIGatewayEvent } from "aws-lambda"
+
+export async function hello (event: APIGatewayEvent) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+        message: "yeh yeah!",
+        params: event.queryStringParameters,
+    }),
+  }
+}
